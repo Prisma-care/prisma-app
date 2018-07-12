abb97743771c0600c90c0d629edd10a94aa740ca
Yens Broothaers <yens.broothaers@student.arteveldehs.be>
2018-07-12 10:29:24 +0200

    Add line under stories

--- resources/views/residents/_header.blade.php
+++ resources/views/residents/_header.blade.php
@@ -1,13 +1,14 @@
 <div class="resi-header d-print-none">
 
-<img src="/img/tmp/profile_5.jpg" class="rounded-circle" alt="Charles Feron">
+<!--<img src="/img/tmp/profile_5.jpg" class="rounded-circle" alt="Charles Feron">-->
  <!-- <h1 class="resi-header-displayname">Mr Feron</h1>-->
   <h1 class="resi-header-displayname">@{{resident_name[0]}}</h1>
 </div>
 
 <div class="container d-print-none">
   <nav class="nav resi-nav justify-content-center">
-    <a class=" verhalen nav-link {{ Route::currentRouteNamed('residents.stories') ? 'active' : '' }}" href="{{ route('residents.stories') }}">Verhalen</a>
+  <h2 id="stories">Verhalen</h2>
+    <!--<a class=" verhalen nav-link {{ Route::currentRouteNamed('residents.stories') ? 'active' : '' }}" href="{{ route('residents.stories') }}">Verhalen</a>-->
     <!--<a class="nav-link {{ Route::currentRouteNamed('residents.preferences') ? 'active' : '' }}" href="{{ route('residents.preferences') }}">Voorkeuren</a>
     <a class="nav-link {{ Route::currentRouteNamed('residents.family') ? 'active' : '' }}" href="{{ route('residents.family') }}">Familie</a>
     <a class="nav-link {{ Route::currentRouteNamed('residents.profile.edit') ? 'active' : '' }}" href="{{ route('residents.profile.edit') }}">Profiel</a>-->

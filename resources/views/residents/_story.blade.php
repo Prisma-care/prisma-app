7f7323a6c6628d848ebf675ac12987cbfc9e897c
Frederik Vincx <frederik.vincx@gmail.com>
2018-07-13 17:23:23 +0200

    Add buttons to add and edit story info

--- resources/views/residents/_story.blade.php
+++ resources/views/residents/_story.blade.php
@@ -12,10 +12,10 @@ class="btn btn-play d-flex btn-primary justify-content-center align-items-center
 </button>
 
 <button
-  @click="index = story['fields']['index']"
-  v-if="story['fields']['type']=='image'"
-  class="btn btn-zoom d-flex btn-primary justify-content-center align-items-center" >
-  <i class="material-icons md-32 mx-auto">zoom_out_map</i>
+@click="index = story['fields']['index']"
+v-if="story['fields']['type']=='image'"
+class="btn btn-zoom d-flex btn-primary justify-content-center align-items-center" >
+<i class="material-icons md-32 mx-auto">zoom_out_map</i>
 </button>
 
 <div class="card-img-overlay">
@@ -26,14 +26,14 @@ class="btn btn-play d-flex btn-primary justify-content-center align-items-center
   data-toggle="modal"
   data-target="#editStoryModal" 
   class="btn btn-link btn-sm px-0 d-flex align-items-center btn-edit">
-  <i class="material-icons md-18">edit</i> <span>Bewerk tekst</span>
+  <i class="material-icons md-18">edit</i> <span>Bewerk</span>
 </button>
 
 </div>
 </div>
 <p 
 v-if="story['fields']['type']=='image' || story['fields']['type']=='youtube'"
-class="mt-3 mb-5 ">@{{ story['fields']['Notes'] }}</p>
+class="mt-3">@{{ story['fields']['Notes'] }}</p>
 
 <div class="card card_text" v-if="story['fields']['type']=='text'">
 
@@ -46,7 +46,35 @@ class="mt-3 mb-5 ">@{{ story['fields']['Notes'] }}</p>
     data-toggle="modal"
     data-target="#editStoryModal" 
     class="btn btn-link btn-sm px-0 btn-edit">
-    <i class="material-icons md-18">edit</i> <span>Bewerk tekst</span>
+    <i class="material-icons md-18 mr-2">edit</i> <span>Pas tekst aan</span>
   </button>
 </div>
+</div>
+
+{{-- CTA's --}}
+<div v-if="story['fields']['Notes'] && !story['fields']['thumbnail']">
+<button
+data-toggle="modal"
+data-target="#editStoryModal"
+class="btn btn-add d-flex justify-content-center align-items-center">
+<i class="material-icons md-18 mr-2">add_photo_alternate</i> <span>Voeg beeld toe</span>
+</button>
+</div>
+
+<div v-if="story['fields']['Notes']">
+  <button
+  data-toggle="modal"
+  data-target="#editStoryModal"
+  class="btn btn-edit d-flex justify-content-center align-items-center">
+  <i class="material-icons md-18 mr-2">edit</i> <span>Pas tekst aan</span>
+</button>
+</div>
+
+<div v-if="!story['fields']['Notes']">
+  <button
+  data-toggle="modal"
+  data-target="#editStoryModal"
+  class="btn btn-add d-flex justify-content-center align-items-center">
+  <i class="material-icons md-18 mr-2">edit</i> <span>Voeg tekst toe</span>
+</button>
 </div>
